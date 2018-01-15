b98b82549e4f04eb91c4b256f827ce3eb5741faf
v-tec2706 <35372433+v-tec2706@users.noreply.github.com>
2018-01-15 01:55:12 +0100

    ''

--- src/Zad2.hs
+++ src/Zad2.hs
@@ -1,11 +1,11 @@
-module Zad2 where
+module Zad2 (solve2_1,solve2_2) where
 
-import Data.Char
+import Data.Char 
 toLines :: String -> [String]
 toLines stringFile = lines stringFile
 
 wordsExtract :: String -> [[String]]
-wordsExtract line = map words $ toLines line
+wordsExtract line = map words $ toLines line 
 
 toIntConvert :: String -> [[Int]]
 toIntConvert line = map (map strToInt) $ wordsExtract line
@@ -19,13 +19,16 @@ diff list = (maximum list) - (minimum list)
 findDiffs :: String -> [Int]
 findDiffs line = map diff $ toIntConvert line
 
-checksum :: String -> Int
-checksum line= sum $ findDiffs line
+solve2_1 :: String -> Int
+solve2_1 line= sum $ findDiffs line
 
-main :: IO ()
-main = do
-	input <-readFile "input2.txt"
-	let sum = checksum input
-	putStrLn $show $ sum
+--part2
+solve2_2 :: String -> Int
+solve2_2 line= sum $ map evenlyDivisible $ toIntConvert line
+
+evenlyDivisible :: [Int] -> Int
+evenlyDivisible ns =maximum $ map (\(x,y) -> x`div`y) $ filter (\(x,y) -> x `mod`y == 0) [ (x, y) | x <- ns, y <- ns, x /= y ]
+
+
 
 
