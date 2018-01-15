36d5671c332f054fe769aebb7040be8681a3fdc7
Aleksander Mikucki <mikucki@gmail.com>
2018-01-15 03:23:16 +0100

    fixed missing zad7

--- test/Zad7Spec.hs
+++ test/Zad7Spec.hs
@@ -1,8 +1,8 @@
 module Zad7Spec(testsZad7Spec) where
 
 import Test.HUnit
-import Zad24
-import Zad24_2
+import Zad7
+--import Zad7_2
 
 test1 = TestCase (assertEqual "solve" "xegshds" $ solve "input7.txt")
 
